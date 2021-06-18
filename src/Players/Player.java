9e0d969d7367ceda46403a9c24715252a49bf394
David <dav.llwyr@gmail.com>
2021-06-18 12:19:10 -0600

    create repo,  player.java, main.java, arrayplayers.java

---
+++ src/Players/Player.java
@@ -0,0 +1,16 @@
+public class Player{
+    private int id,score;
+    private String name;
+    public Player(int id, String name){
+        score = 0;
+        id = id
+        name = name;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getScore(){
+        
+    }
+
+}
