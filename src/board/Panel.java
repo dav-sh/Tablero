36ff536336a8c02e6d68d70c564718f9a58dff1b
David <dav.llwyr@gmail.com>
2021-06-18 16:16:41 -0600

    add pieces

--- src/board/Panel.java
+++ src/board/Panel.java
@@ -9,10 +9,13 @@ public class Panel {
         System.out.println(" ");
         for (int i=0; i<rows; i++) {  //row
             for (int j=0; j<columns; j++){ //column
+                
+                
                 if(i%2==0){ //par row
                     if(j%2==0){ //par column
                         Box b = new Box();
                         //System.out.print(b.color(5)+b.getCell()+b.color(0));
+                        returnCell(i, b);
                         System.out.print(b.getCell());
                     }else{ //odd column
                         Box c = new Box();
@@ -25,6 +28,7 @@ public class Panel {
                         System.out.print(b.getSpace());
                     }else{ //odd column
                         Box c = new Box();
+                        returnCell(i,c);
                         System.out.print(c.getCell());
                     }
                 }
@@ -34,6 +38,14 @@ public class Panel {
         System.out.println(" ");
 
     }
-
+    public void returnCell(int i, Box b){
+        if(i==0 || i==2 || i==1){
+            b.setCell("|xx|");
+        }else if(i==3 || i==4){
+            b.setCell("||||");
+        }else if(i==6 || i==7 || i==5){
+            b.setCell("|yy|");
+        }
+    }
 
 }
